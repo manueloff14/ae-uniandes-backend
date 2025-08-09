1f3167c4cdf80221a348e8212f1d3c11d6c533d0
Manuel Cabrera <107600099+manueloff14@users.noreply.github.com>
2025-08-09 16:06:38 -0500

    Update plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,21 @@
-module.exports = () => ({});
+// config/plugins.js
+module.exports = ({ env }) => ({
+  // Habilita el plugin para poder usar `?populate=deep`
+  'populate-deep': {
+    enabled: true,
+    config: {
+      defaultDepth: 5, // ajusta si necesitas más/menos niveles
+    },
+  },
+
+  // 👇 Ejemplos de otros plugins (opcional). Borra si no los usas.
+  // 'i18n': {
+  //   enabled: true,
+  //   config: { defaultLocale: 'es' },
+  // },
+  // 'upload': {
+  //   config: {
+  //     breakpoints: { large: 1000, medium: 750, small: 500, thumbnail: 245 },
+  //   },
+  // },
+});
